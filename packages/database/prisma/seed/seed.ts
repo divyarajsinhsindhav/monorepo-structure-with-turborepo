d147adf0760d0c5aa0206a16767cb70e45cd9afd
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2025-07-27 16:52:04 +0530

    Add prisma database

---
+++ packages/database/prisma/seed/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.createMany({
+        data: [
+            { name: 'Alice', email: 'alice@example.com' },
+            { name: 'Bob', email: 'bob@example.com' },
+        ],
+    })
+
+    console.log('✅ Seeded database.')
+}
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
